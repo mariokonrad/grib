9be018342fad499e07ebf67cbc52b291c654a2b8
Mario Konrad <mario.konrad@gmx.net>
2011-02-10 00:47:15 +0100

    code cleanup

--- libgrib/conv_float.c
+++ libgrib/conv_float.c
@@ -2,61 +2,60 @@
 #include <bits.h>
 #include <math.h>
 
-double ibm2real(unsigned char * buf, size_t off)
+/* IBM 32bit single precision
+ * sAAAAAAA BBBBBBBB BBBBBBBB BBBBBBBB
+ * R = (-1)^s * 2^(-24) * B * 16^(A-64)
+ */
+
+double ibm2real(const unsigned char * buf, unsigned int off)
 {
-	int sign;
-	int exp;
-	int fr;
-	double native_real;
+	int s;
+	int e;
+	int b;
 
-	get_bits(buf, &sign, off, 1);
-	get_bits(buf, &exp, off+1, 7);
-	exp -= 64;
-	get_bits(buf, &fr, off+8, 24);
-	native_real = pow(2.0, -24.0) * (double)fr * pow(16.0, (double)exp);
-
-	return (sign == 1) ? -native_real : native_real;
+	get_bits(buf, &s, off + 0,  1);
+	get_bits(buf, &e, off + 1,  7);
+	get_bits(buf, &b, off + 8, 24);
+	return pow(2.0, -24.0) * (double)(s ? -b : b) * pow(16.0, (double)e - 64);
 }
 
 int32_t ieee2ibm(double ieee)
 {
-	union {
-		int32_t ibm_real;
-		unsigned char ir[sizeof(ibm_real)];
-	} value;
-
-
 	int32_t ibm_real = 0;
 	unsigned char * ir = (unsigned char *)&ibm_real;
-	int sign = 0;
-	int fr = 0;
-	int exp = 64;
+
+	int s = 0;
+	int b = 0;
+	int e = 64;
 	const double full = 0xffffff;
-	size_t size = sizeof(size_t) * 8;
-	size_t off = 0;
+	unsigned int off = 0;
 
 	if (ieee != 0.0) {
 		if (ieee < 0.0) {
-			sign = 1;
+			s = 1;
 			ieee = -ieee;
 		}
 		ieee /= pow(2.0, -24.0);
-		while (exp > 0 && ieee < full) {
+		while (e > 0 && ieee < full) {
 			ieee *= 16.0;
-			exp--;
+			e--;
 		}
 		while (ieee > full) {
 			ieee /= 16.0;
-			exp++;
+			e++;
 		}
-		fr = ieee + 0.5;
-		if (size > 32) {
-			off = size - 32;
+		b = ieee + 0.5;
+
+		/*
+		if (sizeof(ibm_real)*8 > 32) {
+			off = sizeof(ibm_real)*8 - 32;
 			set_bits(ir, 0, 0, off);
 		}
-		set_bits(ir, sign, off, 1);
-		set_bits(ir, exp, off + 1, 7);
-		set_bits(ir, fr, off + 8, 24);
+		*/
+
+		set_bits(ir, s, off + 0,  1);
+		set_bits(ir, e, off + 1,  7);
+		set_bits(ir, b, off + 8, 24);
 	}
 	return ibm_real;
 }
--- libgrib/conv_float.h
+++ libgrib/conv_float.h
@@ -1,14 +1,13 @@
 #ifndef __CONV_FLOAT__H__
 #define __CONV_FLOAT__H__
 
-#include <stdio.h>
 #include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-double ibm2real(unsigned char * buf, size_t off);
+double ibm2real(const unsigned char * buf, unsigned int off);
 int32_t ieee2ibm(double ieee);
 
 #ifdef __cplusplus
