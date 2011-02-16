4b3e96abf763e4dbeb5032c0fbf1cf12c1626dde
Konrad <konradm1@TROULWD0057.rok.roche.com>
2011-02-16 12:03:31 +0100

    demo how to read forecast for different points in time

--- grib.cpp
+++ grib.cpp
@@ -654,19 +654,21 @@ static int write_func(const void * buf, unsigned int len, void * ptr)
 
 int main(int, char **)
 {
-	for (int i = 0; i < 6; i += 3) {
-		GRIB grib(GRIB::GRID_2P5);
-		grib.set(GRIB::Region(35, 5, -15, 15));
-		grib.set(GRIB::CYCLE_00);
+	GRIB grib(GRIB::GRID_2P5);
+	grib.set(GRIB::Region(35, 5, -15, 15));
+	grib.set(GRIB::CYCLE_00);
+	grib.add_level(GRIB::LEVEL_10_M_ABOVE_GROUND);
+	grib.add_level(GRIB::LEVEL_MEAN_SEA_LEVEL);
+	grib.add_level(GRIB::LEVEL_SURFACE);
+	grib.add_var(GRIB::VAR_GUST);
+	grib.add_var(GRIB::VAR_PRMSL);
+	grib.add_var(GRIB::VAR_UGRD);
+	grib.add_var(GRIB::VAR_VGRD);
+
+	GRIB::Data grib2;
+
+	for (int i = 0; i < 24; i += 6) {
 		grib.set_time(i);
-		grib.add_level(GRIB::LEVEL_10_M_ABOVE_GROUND);
-		grib.add_var(GRIB::VAR_GUST);
-		grib.add_var(GRIB::VAR_HGT);
-		grib.add_var(GRIB::VAR_PRES);
-		grib.add_var(GRIB::VAR_PRMSL);
-		grib.add_var(GRIB::VAR_TMP);
-		grib.add_var(GRIB::VAR_UGRD);
-		grib.add_var(GRIB::VAR_VGRD);
 
 		GRIB::Data data;
 		printf("url: [%s]\n", grib.url().c_str());
@@ -674,27 +676,28 @@ int main(int, char **)
 			printf("ERROR: %s\n", GRIB::error_string(errno).c_str());
 			break;
 		}
+		std::copy(data.begin(), data.end(), std::back_inserter(grib2));
+	}
 
-		GRIB::Data grib1;
-		GRIB::DataRange range(data.begin(), data.end());
-		grib2_to_grib1_conv(read_func, &range, write_func, &grib1);
+	GRIB::Data grib1;
+	GRIB::DataRange range(grib2.begin(), grib2.end());
+	grib2_to_grib1_conv(read_func, &range, write_func, &grib1);
 
-		if (data.size()) {
-			std::ofstream ofs("test.grb2");
-			if (!ofs) {
-				printf("ERROR: cannot open file\n");
-				break;
-			}
-			std::copy(data.begin(), data.end(), std::ostream_iterator<GRIB::Data::value_type>(ofs));
+	if (grib2.size()) {
+		std::ofstream ofs("test.grb2");
+		if (ofs) {
+			std::copy(grib2.begin(), grib2.end(), std::ostream_iterator<GRIB::Data::value_type>(ofs));
+		} else {
+			printf("ERROR: cannot open file\n");
 		}
+	}
 
-		if (grib1.size()) {
-			std::ofstream ofs("test.grb1");
-			if (!ofs) {
-				printf("ERROR: cannot open file\n");
-				break;
-			}
+	if (grib1.size()) {
+		std::ofstream ofs("test.grb1");
+		if (ofs) {
 			std::copy(grib1.begin(), grib1.end(), std::ostream_iterator<GRIB::Data::value_type>(ofs));
+		} else {
+			printf("ERROR: cannot open file\n");
 		}
 	}
 
